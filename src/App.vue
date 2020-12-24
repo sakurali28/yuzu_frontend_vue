f575a3cf5ade81e39d57c62ddeaaabda283241d8
YOUR NAME <YOUR@EMAIL.com>
2020-12-23 23:50:07 -0600

    updates navbar

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,33 @@
   <div id="app">
     <div id="nav" v-if="isLoggedIn()">
       <nav class="navbar navbar-dark bg-dark">
-        <a class="navbar-brand" href="/">YUZU</a>
+        <a class="navbar-brand" href="/recipes">YUZU</a>
+        <ul class="nav">
+          <li class="nav-item">
+            <a class="nav-link" href="/recipes">Recipes</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Tags</a>
+            <div class="dropdown-menu">
+              <div class="dropdown-item" v-for="tag in tags">
+                <router-link v-bind:to="`/tags/${tag.id}`">
+                  <p>{{ tag.name }}</p>
+                </router-link>
+              </div>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" href="/tags/new">+ Tag</a>
+            </div>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/recipes/new">+ Recipe</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/users">Profile</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/logout">Logout</a>
+          </li>
+        </ul>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
@@ -26,7 +52,24 @@
 </style>
 
 <script>
+import axios from "axios";
+
 export default {
+  data: function() {
+    return {
+      tags: [],
+    };
+  },
+
+  created: function() {
+    axios
+      .get("/api/tags")
+      .then(response => {
+        console.log("tag index", response);
+        this.tags = response.data;
+      });
+  },
+
   methods: {
     isLoggedIn: function() {
       if (localStorage.getItem("jwt")) {
