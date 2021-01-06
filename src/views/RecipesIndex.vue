89c30d33a88484d95a8419f0d5b8353ac666a1f4
YOUR NAME <YOUR@EMAIL.com>
2021-01-06 13:23:31 -0600

    updates RecipesIndex cards

--- src/views/RecipesIndex.vue
+++ src/views/RecipesIndex.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="recipes">
+  <!-- <div class="recipes">
     <h2>Recipes</h2>
     <div>
       Search: <input v-model="titleFilter">
@@ -23,6 +23,45 @@
     <ul>
       <li v-for="error in errors">{{ error }}</li>
     </ul>
+  </div> -->
+
+  <div id="wrapper" class="clearfix">
+    <section id="content">
+      <div class="content-wrap pt-5" style="overflow: visible;">
+        <div class="container">
+          <h3 class="mb-0">Recipes</h3>
+
+          <!-- search bar -->
+          <div class="widget widget-search">
+            <form class="input-group">
+              <input class="form-control" type="search" placeholder="Search" aria-label="Search" v-model="titleFilter">
+              <div class="input-group-append">
+                <button class="btn btn-outline-secondary icon-line-search" type="button"></button>
+              </div>
+            </form>
+          </div>
+
+
+          <div class="row posts-md">
+            <div class="col-lg-3 col-sm-6" v-for="recipe in filterBy(recipes, titleFilter, 'name', 'ingredients', 'tag')">
+              <article class="entry">
+
+                <div class="entry-image">
+                  <a><img v-bind:src="`${recipe.image}`" alt="Image 3"></a>
+                </div>
+
+                <div class="entry-title title-sm text-left">
+                  <div class="entry-meta"><i class="icon-line-clock"></i> {{ recipe.cooktime_conversion }}</div>
+                  <h3><a v-bind:href="`/recipes/${recipe.id}`" class="color-underline stretched-link">{{ recipe.name }}</a></h3>
+                </div>
+
+              </article>
+            </div>
+            
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
@@ -57,6 +96,7 @@ export default {
         .then(response => {
           this.recipes = response.data;
           console.log("All Recipes", this.recipes);
+
         });
     },
   },
